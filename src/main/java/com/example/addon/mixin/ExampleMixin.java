81043a949b35fc7b9dc14a8976da490d95a729a1
SmilerRyan <ryanthornton03@gmail.com>
2026-08-29 06:21:42 +0100

    I restored the missing ExampleMixin as an empty compatibility mixin targeting Minecraft. No modules were removed or disabled.

---
+++ src/main/java/com/example/addon/mixin/ExampleMixin.java
@@ -0,0 +1,12 @@
+package com.example.addon.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Empty compatibility mixin retained because addon-template.mixins.json is
+ * registered by fabric.mod.json. The original template referenced this class.
+ */
+@Mixin(Minecraft.class)
+public abstract class ExampleMixin {
+}
